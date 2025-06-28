2ffb160cd354c66f698c47324ccfdb3e9e9f1a2b
Samuel <samuel@thegreg.ai>
2025-06-28 08:32:35 +0000

    cors update

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const app = express()
 await connectDB()
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:1856",
     credentials: true
 }))
 app.use(cookieParser());
