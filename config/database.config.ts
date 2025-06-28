509f47ff495d0d7987d119af0d9b4f0b00b8a390
Samuel <samuel@thegreg.ai>
2025-06-28 07:43:10 +0000

    first commit

--- config/database.config.ts
+++ config/database.config.ts
@@ -2,11 +2,11 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017')
+        await mongoose.connect(`mongodb+srv://samuel-yeboah:${process.env.MONGO_DB_PASSWORD}@kwabscluster.d7jjk.mongodb.net/users/?retryWrites=true&w=majority&appName=KwabsCluster`)
         console.log("mongodb connection - success")
     } catch (error) {
         console.error("error connecting to mongodb", error)
     }
 }
 
-export {connectDB}
+export { connectDB }
--- routes/router.ts
+++ routes/router.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import { signupHandler,signinHandler, googleAuthServer, googleResourceServer, getUserProfile} from "../controllers/auth.controllers.ts";
+import { signupHandler, signinHandler, googleAuthServer, googleResourceServer, getUserProfile } from "../controllers/auth.controllers.ts";
 import { verifyToken } from "../middlewares/verifyToken.ts";
 
 
 const router = Router();
 
 
-router.post("/auth/signin", signinHandler);     
-router.post("/auth/signup", signupHandler);  
+router.post("/auth/signin", signinHandler);
+router.post("/auth/signup", signupHandler);
 router.get("/users/me", verifyToken, getUserProfile); // Now includes both middleware and handler
 router.get("/auth/google", googleAuthServer)
-router.get("/auth/callback",googleResourceServer)
+router.get("/auth/callback", googleResourceServer)
 
 export default router
+
